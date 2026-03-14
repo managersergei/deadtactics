5e2527100ca49f961442571def9c85c01f49b9b4
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2026-03-14 20:49:15 +0500

    fix: удалить styles.css, убрать bottom:0, удалить main.js

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import { uiConstants, gameConfig } from './config/ui-constants.js';
-import { state, helpers } from './core/state.js';
-
-// Используйте импортированные объекты напрямую
-console.log(uiConstants.SOME_CONSTANT);
-console.log(gameConfig.SOME_CONFIG);
-console.log(state.someStateVariable);
-console.log(helpers.someHelperFunction());
